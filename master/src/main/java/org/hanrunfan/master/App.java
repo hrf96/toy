9b9b9369bc8f98c4639e9537e3a2595ab5325841
hanrunfan <hrf96@163.com>
2014-03-25 11:31:38 +0800

    初始化

---
+++ master/src/main/java/org/hanrunfan/master/App.java
@@ -0,0 +1,13 @@
+package org.hanrunfan.master;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
